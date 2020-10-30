84271d33410c6aee3f0d963c87038ccf412c5f3a
Jerin Ignatious Palliparambil <jerinignatious1998@gmail.com>
2020-10-31 00:51:45 +0530

    components separated

---
+++ src/Anime.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Anime = (props) => {
+  const {img, title, rating} = props;
+  return (
+  <article className='anime'>
+    <img src={img}/>
+    <h2>{title}</h2>
+    <h4>{rating}</h4>
+  </article>
+  );
+};
+
+export default Anime

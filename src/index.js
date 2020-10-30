a443e01e21d733f389ec8bbbe6a2d9ee06cca656
Jerin Ignatious Palliparambil <jerinignatious1998@gmail.com>
2020-10-30 12:41:09 +0530

    initial commit

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+function Greeting(){
+  return (
+    <div>
+      <Person/>
+      <Message/>
+    </div>
+  );
+}
+
+const Person = () => <h3>Hello World</h3>;
+const Message = () => {
+  return (
+    <p>
+      This is my message
+    </p>
+  );
+};
+
+ReactDom.render(<Greeting/>, document.getElementById('root'));

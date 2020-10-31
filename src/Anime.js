a1f3768789953e189b7250d69933c6eb620b1588
Jerin Ignatious Palliparambil <jerinignatious1998@gmail.com>
2020-10-31 15:03:49 +0530

    Update Anime.js

--- src/Anime.js
+++ src/Anime.js
@@ -4,7 +4,7 @@ const Anime = (props) => {
   const {img, title, rating} = props;
   return (
   <article className='anime'>
-    <img src={img}/>
+    <img src={img} alt=''/>
     <h2>{title}</h2>
     <h4>{rating}</h4>
   </article>
